3d93514f68f762e202e173bc61062b3108bca361
Leo G. <95503088+LamaGame@users.noreply.github.com>
2025-03-04 21:37:14 +0100

    fix

--- prices.js
+++ prices.js
@@ -1,9 +1,10 @@
 const itemPrices = {
     "Gameboy CLASSIC": 99.95,
-    "Nintendo Entertainment System": 399.90,
+    "Nintendo Entertainment System": 399.95,
     "Produkt 3": 30.00,
     "Produkt 4": 40.00,
     "Produkt 5": 50.00,
     "Produkt 6": 60.00,
-    "Produkt 7": 70.00
+    "Produkt 7": 70.00,
+    "Nintendo Switch": 299.99,
 };
